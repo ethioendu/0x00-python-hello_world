69e03960a186341a6833b2b4cf671ae14523573b
ethioenud <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@github.com>
2024-01-09 13:08:53 -0800

    commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
-"""
-Module with a script that adds arguments
-to a Python list saved in a file
-"""
-
+'''a script that adds all arguments to a Python list then save them to a file
+'''
 import sys
-import os
+import json
+import os.path
 
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-
-
-filename = 'add_item.json'
-if os.path.exists(filename):
-    # if it exists, called it, otherwise
-    # make a new one
-    the_list = load_from_json_file(filename)
+filename = "add_item.json"
+if os.path.isfile(filename):
+    obj = load_from_json_file(filename)
 else:
-    with open(filename, mode='w') as f:
-        the_list = []
-        save_to_json_file([], filename)
-
-for arg in range(1, len(sys.argv)):
-    the_list.append(sys.argv[arg])
-    save_to_json_file(the_list, filename)
+    obj = []
+obj.extend(sys.argv[1:])
+save_to_json_file(obj, filename)
