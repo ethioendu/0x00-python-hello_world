b8d35e35604cc6fa469dfda0e179e58d60fd61bc
ethioenud <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@github.com>
2024-01-10 06:30:48 -0800

    5-base_geometry.py

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('5-base_geometry').BaseGeometry
+"""Defines an empty class BaseGeometry."""
 
-bg = BaseGeometry()
 
-print(bg)
-print(dir(bg))
-print(dir(BaseGeometry))
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
