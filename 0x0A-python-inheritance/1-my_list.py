c735a98e57386244a7217a234a0422e95ad3b03c
ethioenud <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@github.com>
2024-01-10 05:39:10 -0800

    1-main.py

--- 0x0A-python-inheritance/1-my_list.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-class MyList(list):
-    """Class with method print_sorted"""
-    pass
-
-    def print_sorted(self):
-        """Methot that sorted a list"""
-
-        print(sorted(list(self)))
