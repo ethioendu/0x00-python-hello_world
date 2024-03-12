dd4d5f045be3a4baaee5e27e9bbdce564287cb47
ethioenud <ethioendu@git.com>
2024-03-12 12:02:53 -0700

    all

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
-// prints the addition of 2 integers
-
-function add (a, b) {
-  return parseInt(a) + parseInt(b);
-}
-
-console.log(add(process.argv[2], process.argv[3]));
+let count = 0;
+exports.logMe = function (item) { console.log(`${count++}: ${item}`); };
