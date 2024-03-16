46a3c4a257f96edce57d42ab5a8fb7e99fdc639a
ethioenud <ethioendu@git.com>
2024-03-16 07:45:52 -0700

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
-# Displays all values in the states table of the database hbtn_0e_0_usa
-# whose name matches that supplied as argument.
-# Usage: ./2-my_filter_states.py <mysql username> \
-#                                <mysql password> \
-#                                <database name> \
-#                                <state name searched>
-import sys
+"""
+a script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+"""
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * \
-                 FROM `states` \
-                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
-    [print(state) for state in c.fetchall()]
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    '''Create a cursor object to execute SQL queries.'''
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
+    '''Fetch all the rows returned by the query.'''
+    rows = cur.fetchall()
+    ''' Display the results.'''
+    for row in rows:
+        print(row)
+
+    '''Close the cursor and database connection.'''
+    cur.close()
+    db.close()
