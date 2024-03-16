a72a91fa4514c2323e35ac9f5e13ab2352554a9a
ethioenud <ethioendu@git.com>
2024-03-16 10:14:43 -0700

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-'''
-a python script that lists all cities
-from the database hbtn_0e_4_usa.
-'''
+""" lists all states from the database hbtn_0e_0_usa
+"""
 import MySQLdb
-import sys
+from sys import argv
 
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    '''Create a cursor object to interact with the database.'''
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
     cur = db.cursor()
-    '''Execute the SQL query to retrieve the cities.'''
-    cur.execute("""SELECT * FROM states WHERE name
-                LIKE BINARY 'N%' ORDER BY states.id""")
-    '''Fetch all the results.'''
+    cur.execute("""
+    SELECT cities.id, cities.name, states.name
+    FROM cities
+    JOIN states
+    ON cities.state_id = states.id
+    ORDER BY cities.id ASC
+    """)
     rows = cur.fetchall()
-    '''Display the results.'''
+
     for row in rows:
         print(row)
