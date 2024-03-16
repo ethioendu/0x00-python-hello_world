43c6c327870b75b7587e29489c97ca18b6f2096f
ethioenud <ethioendu@git.com>
2024-03-16 10:30:54 -0700

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,28 +1,18 @@
 #!/usr/bin/python3
-""" a script that lists all State objects from the database hbtn_0e_6_usa."""
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+"""List all states"""
+from sys import argv
 from model_state import Base, State
-import sys
-
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    """
-    Check if the script is run directly
-    and gather the command-line arguments
-    """
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-
-    """Bind the engine to the Base class"""
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2],
+                argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
-    """Create a session to interact with the database."""
     Session = sessionmaker(bind=engine)
-    session = Session
-
-    """Display the results."""
-    for instance in session.query(State).order_by(State.id):
-        print('{0}: {1}'.format(instance.id, instance.name))
+    session = Session()
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()
