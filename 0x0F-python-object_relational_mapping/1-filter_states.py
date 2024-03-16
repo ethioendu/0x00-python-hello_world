6f31ade757a82b0925032f9ce674dab798c8ae08
ethioenud <ethioendu@git.com>
2024-03-16 10:19:21 -0700

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,43 +1,21 @@
 #!/usr/bin/python3
-"""
-a script that lists all states with a name starting
-with N (upper N) from the database hbtn_0e_0_usa.
+""" lists all states from the database hbtn_0e_0_usa
 """
 import MySQLdb
+from sys import argv
 
 
-def list_states(username, password, database):
-    '''Connect to MySQL server.'''
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    ''' Create a cursor object to execute SQL queries.'''
-    cursor = db.cursor()
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-    ''' Execute the query to fetch states starting with 'N'.'''
+    cur = db.cursor()
+    cur.execute("""
+    SELECT * FROM states
+    WHERE name LIKE BINARY 'N%'
+    ORDER BY states.id ASC
+    """)
+    rows = cur.fetchall()
 
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-
-    '''Fetch all the rows returned by the query.'''
-    rows = cursor.fetchall()
-
-    '''Display the results.'''
     for row in rows:
         print(row)
-
-    '''Close the cursor and database connection.'''
-
-    cursor.close()
-    db.close()
-
-
-'''Entry point of the script.'''
-if __name__ == '__main__':
-    import sys
-
-    '''Get the command-line arguments.'''
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    '''Call the function to list states.'''
-    list_states(username, password, database)
